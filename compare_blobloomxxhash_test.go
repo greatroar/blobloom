156328342d9e88dbbe68254ecc99c7057724ea0f
greatroar <@>
2020-04-25 10:10:09 +0200

    Benchmark against ourselves+xxhash and document the procedure
    Old is with xxhash, new is without.
    
    name                old time/op  new time/op  delta
    Add1e5_1e2-8        31.4ns ± 1%  15.4ns ± 2%  -51.02%  (p=0.001 n=5+10)
    Add1e6_1e2-8        33.3ns ± 2%  18.3ns ± 4%  -45.21%  (p=0.000 n=5+14)
    Add1e7_1e2-8        68.6ns ± 1%  45.9ns ±17%  -33.00%  (p=0.000 n=5+15)
    Add1e8_1e2-8        95.6ns ± 1%  66.0ns ±22%  -30.99%  (p=0.000 n=10+10)
    Add1e5_1e3-8        35.4ns ± 2%  19.2ns ± 2%  -45.74%  (p=0.001 n=5+10)
    Add1e6_1e3-8        37.8ns ± 2%  22.6ns ± 5%  -40.22%  (p=0.000 n=5+15)
    Add1e7_1e3-8        84.3ns ± 1%  69.4ns ± 5%  -17.75%  (p=0.000 n=5+15)
    Add1e8_1e3-8         105ns ± 1%    91ns ± 2%  -13.97%  (p=0.000 n=10+10)
    TestPos1e5_1e2-8    33.6ns ± 4%  17.0ns ± 2%  -49.52%  (p=0.001 n=5+10)
    TestPos1e6_1e2-8    35.0ns ± 1%  18.5ns ± 2%  -47.34%  (p=0.000 n=5+15)
    TestPos1e7_1e2-8    35.0ns ± 1%  18.3ns ± 2%  -47.63%  (p=0.008 n=5+5)
    TestPos1e5_1e3-8    36.6ns ± 1%  20.7ns ± 5%  -43.46%  (p=0.001 n=5+10)
    TestPos1e6_1e3-8    38.9ns ± 1%  23.4ns ± 4%  -39.91%  (p=0.000 n=5+15)
    TestPos1e7_1e3-8    39.5ns ± 1%  24.6ns ± 3%  -37.76%  (p=0.008 n=5+5)
    TestNeg1e5_1e2-8    32.8ns ± 1%  18.6ns ± 3%  -43.42%  (p=0.008 n=5+5)
    TestNeg1e6_1e2-8    36.7ns ± 0%  21.1ns ± 3%  -42.55%  (p=0.000 n=5+20)
    TestNeg1e7_1e2-8    37.6ns ± 4%  20.8ns ± 4%  -44.65%  (p=0.001 n=5+10)
    TestNeg1e6_1e3-8    37.4ns ± 1%  21.8ns ± 2%  -41.84%  (p=0.000 n=5+14)
    TestNeg1e7_1e3-8    37.0ns ± 1%  21.4ns ± 1%  -42.05%  (p=0.008 n=5+5)
    TestEmpty1e5_1e2-8  22.1ns ± 1%   8.8ns ± 4%  -60.00%  (p=0.000 n=5+11)
    TestEmpty1e6_1e2-8  24.5ns ± 1%   9.9ns ± 4%  -59.69%  (p=0.000 n=5+20)
    TestEmpty1e7_1e2-8  27.8ns ± 4%  11.3ns ± 2%  -59.42%  (p=0.001 n=5+10)
    TestEmpty1e5_1e3-8  22.7ns ± 1%   9.0ns ± 1%  -60.38%  (p=0.000 n=5+11)
    TestEmpty1e6_1e3-8  24.5ns ± 1%  10.1ns ± 3%  -58.99%  (p=0.000 n=5+19)
    TestEmpty1e7_1e3-8  28.8ns ± 3%  12.3ns ± 3%  -57.19%  (p=0.001 n=5+10)
    TestPos1e8_1e2-8    35.1ns ± 1%  18.7ns ± 1%  -46.81%  (p=0.008 n=5+5)
    TestPos1e8_1e3-8    40.1ns ± 0%  24.6ns ± 4%  -38.65%  (p=0.008 n=5+5)
    TestNeg1e8_1e2-8    37.4ns ± 1%  21.9ns ± 0%  -41.34%  (p=0.016 n=5+4)

---
+++ compare_blobloomxxhash_test.go
@@ -0,0 +1,41 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build benchcompare blobloomxxhash
+// +build ignore
+
+package blobloom_test
+
+import (
+	"github.com/cespare/xxhash/v2"
+	"github.com/greatroar/blobloom"
+)
+
+type bloomFilter blobloom.Filter
+
+func (f *bloomFilter) Add(hash []byte) {
+	h := xxhash.Sum64(hash)
+	((*blobloom.Filter)(f)).Add(h)
+}
+
+func (f *bloomFilter) Has(hash []byte) bool {
+	h := xxhash.Sum64(hash)
+	return ((*blobloom.Filter)(f)).Has(h)
+}
+
+func newBF(capacity int, fpr float64) *bloomFilter {
+	f := blobloom.NewOptimized(blobloom.Config{
+		Capacity: uint64(capacity),
+		FPRate:   fpr,
+	})
+	return (*bloomFilter)(f)
+}
