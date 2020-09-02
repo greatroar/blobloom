fb2ecdfbc0d5a2a4024fb6e7c4eb1dd478fa5372
greatroar <@>
2020-09-02 09:04:33 +0200

    Remove old build tag from benchmarks

--- benchmark_test.go
+++ benchmark_test.go
@@ -10,7 +10,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build !benchcompare
+// Benchmarks for the basic operations live in the benchmarks/ subpackage.
 
 package blobloom
 
