09cb8ecd1b8d70d543be74df50e41ad3cd4f0b01
greatroar <61184462+greatroar@users.noreply.github.com>
2021-10-30 16:50:11 +0200

    Use 64-bit bulk ops on arm64
    Benchmark results on Raspberry Pi 4B running Linux, Go 1.17.1:
    
    name                 old time/op  new time/op  delta
    CardinalityDense-4    314µs ± 0%   314µs ± 0%     ~     (p=0.494 n=10+10)
    CardinalitySparse-4   204µs ± 0%   204µs ± 0%     ~     (p=0.579 n=10+10)
    Onescount-4          56.6ns ± 0%  56.6ns ± 0%     ~     (p=0.342 n=10+9)
    Union-4              4.75ms ± 1%  4.08ms ± 0%  -14.20%  (p=0.000 n=8+7)

--- setop_64bit.go
+++ setop_64bit.go
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !nounsafe
+//go:build (amd64 || arm64) && !nounsafe
+// +build amd64 arm64
 // +build !nounsafe
 
 package blobloom
--- setop_other.go
+++ setop_other.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !amd64 || nounsafe
-// +build !amd64 nounsafe
+//go:build (!amd64 && !arm64) || nounsafe
+// +build !amd64,!arm64 nounsafe
 
 package blobloom
 
